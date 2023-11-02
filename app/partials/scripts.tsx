8a3c92ab358ca176c5bdf7bfa267220a43f2137a
adiapr <da801100@gmail.com>
2023-11-02 09:46:05 +0700

    scriptys

--- app/partials/scripts.tsx
+++ app/partials/scripts.tsx
@@ -1,21 +1,23 @@
-import React from 'react'
+"use client"
+import { useEffect } from 'react';
+import Script from 'next/script';
 
 export default function Scripts() {
+  useEffect(() => {
+    // Your script initialization or any other logic that depends on the loaded scripts can go here.
+  }, []);
+
   return (
     <div>
-        
-        {/* <!-- Vendor JS Files --> */}
-        <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
-        <script src="assets/vendor/aos/aos.js"></script>
-        <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-        <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
-        <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-        <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
-        <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
-        <script src="assets/vendor/php-email-form/validate.js"></script>
-
-        {/* <!-- Template Main JS File --> */}
-        <script src="assets/js/main.js"></script>
+      <Script src="assets/vendor/purecounter/purecounter_vanilla.js" strategy="afterInteractive" />
+      <Script src="assets/vendor/aos/aos.js" strategy="afterInteractive" />
+      <Script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js" strategy="afterInteractive" />
+      <Script src="assets/vendor/glightbox/js/glightbox.min.js" strategy="afterInteractive" />
+      <Script src="assets/vendor/isotope-layout/isotope.pkgd.min.js" strategy="afterInteractive" />
+      <Script src="assets/vendor/swiper/swiper-bundle.min.js" strategy="afterInteractive" />
+      <Script src="assets/vendor/waypoints/noframework.waypoints.js" strategy="afterInteractive" />
+      <Script src="assets/vendor/php-email-form/validate.js" strategy="afterInteractive" />
+      <Script src="assets/js/main.js" strategy="afterInteractive" />
     </div>
-  )
+  );
 }
